88c244439434b788b96201c4ba7642a6befdf017
fazrisuhada007 <fazrisuhada3@gmail.com>
2025-08-26 20:51:55 +0700

    React Dasar: Component

--- react-dasar/src/hello-world/HelloWorld.jsx
+++ react-dasar/src/hello-world/HelloWorld.jsx
@@ -1,8 +1,24 @@
-export function HelloWorld(){
-    return(
-        <div>
-            <h1>Hello World</h1>
-            <p>Selamat belajar react.js</p>
-        </div>
-    )
+export function HelloWorld() {
+  return (
+    <div>
+        <HeaderHelloWorld/>
+        <ParagraphHelloWorld/>
+    </div>
+  )
+}
+
+export function HeaderHelloWorld() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export function ParagraphHelloWorld() {
+  return (
+    <div>
+      <p>Selamat belajar react.js</p>
+    </div>
+  )
 }

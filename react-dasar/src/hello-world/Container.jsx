f32486a050bf98dff5197016d44f724a06297837
fazrisuhada007 <fazrisuhada3@gmail.com>
2025-08-27 19:43:49 +0700

    React Dasar: Nested Component

---
+++ react-dasar/src/hello-world/Container.jsx
@@ -0,0 +1,14 @@
+export default function Container({children}){
+    return(
+        <div>
+            <h1>
+                Navbar
+            </h1>
+            {children}
+            <footer>
+                React Js 2025
+            </footer>
+        </div>
+
+    )
+}

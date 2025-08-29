83cf43774d1abb79706927b5250d177dbfafde7d
fazrisuhada007 <fazrisuhada3@gmail.com>
2025-08-29 21:16:09 +0700

    React Dasar : Event Handler

--- react-dasar/src/hello-world/main.jsx
+++ react-dasar/src/hello-world/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import { HelloWorld } from "./HelloWorld";
 import Container from "./Container";
 import TodoList from "../todo-list/TodoList";
+import AlertButton from "../button/AlertButton";
+import MyButton from "../button/MyButton";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Container>
       <HelloWorld />
       <TodoList/>
+      <AlertButton text={"click me"} message={"Selamat Datang"}/>
+      <MyButton text={"click me"} onSmash={()=>alert("Semoga Harimu Menyenangkan")}/>
     </Container>
   </StrictMode>
 );

9c700d2fce8324f8eee10f4d31007c67253cca6e
Fazri Suhada <fazrisuhada56@gmail.com>
2025-09-18 04:23:12 +0700

    react dasar : update state

--- react-dasar/src/form/Counter.jsx
+++ react-dasar/src/form/Counter.jsx
@@ -2,17 +2,28 @@ import { useState } from "react";
 
 export default function Counter() {
   let [counter, setCounter] = useState(0);
-//   let [counter2, setCounter2] = useState(0);
-
+  let [data, setData] = useState(0);
 
   function handleClick() {
     setCounter(counter + 1);
   }
 
+  function handleClickPlus3() {
+    setData((e) => e + 1);
+    setData((e) => e + 1);
+    setData((e) => e + 1);
+  }
+
   return (
-    <div style={{marginTop:"10px"}}>
-      <button onClick={handleClick}>Increment</button>
-      <h1>Counter : {counter}</h1>
+    <div style={{ marginTop: "10px" }}>
+      <div>
+        <button onClick={handleClick}>Increment</button>
+        <h1>Counter : {counter}</h1>
+      </div>
+      <div>
+        <button onClick={handleClickPlus3}>Counter + 3</button>
+        <h1>Counter : {data}</h1>
+      </div>
     </div>
   );
 }

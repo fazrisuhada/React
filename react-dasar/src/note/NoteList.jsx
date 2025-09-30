ddbabd62e43fec94c910291ddb273288bc2e017c
Fazri Suhada <fazrisuhada56@gmail.com>
2025-10-01 06:08:26 +0700

    react dasar : memo

--- react-dasar/src/note/NoteList.jsx
+++ react-dasar/src/note/NoteList.jsx
@@ -1,16 +1,34 @@
-import { useContext } from "react"
+import { useContext, useMemo, useState } from "react"
 import Note from "./Note"
 import { NotesContext } from "./NoteContext"
+import { useRef } from "react"
 
 export default function NoteList(){
     const notes = useContext(NotesContext)
+    const [search, setSearch] = useState('')
+    const searchInput = useRef(null)
+
+    const filteredNotes = useMemo(() => {
+        return notes.filter(note => note.text.toLowerCase().includes(search.toLowerCase()))
+    }, [notes, search])
+
+    const handleSearch = () => {
+        setSearch(searchInput.current.value)
+    }
+
     return(
-        <ul>
-            {notes.map(note =>(
-                <li key={note.id}>
-                    <Note note={note} />
-                </li>
-            ))}
-        </ul>
+        <div>
+            <div>
+                <input type="text" ref={searchInput} onChange={handleSearch} />
+                {/* <button onClick={handleSearch}>Search</button> */}
+            </div>
+            <ul>
+                {filteredNotes.map(note =>(
+                    <li key={note.id}>
+                        <Note note={note} />
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }

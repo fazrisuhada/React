25703373aa81caac79b7eae4441380b6a2bce84b
Fazri Suhada <fazrisuhada56@gmail.com>
2025-12-12 11:04:39 +0700

    react router : index nested

---
+++ react-router/src/Data.jsx
@@ -0,0 +1,8 @@
+export default function Data() {
+    return (
+        <div>
+            <h1>Data Page</h1>
+            <p>Welcome to the Data Page</p>
+        </div>
+    )
+}
--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -8,6 +8,7 @@ import About from './About.jsx'
 import Product from './Product.jsx'
 import Costumer from './Costumer.jsx'
 import Seller from './Seller.jsx'
+import Data from './Data.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/data">
+          <Route index element={<Data />} />
           <Route path="product" element={<Product />} />
           <Route path="customer" element={<Costumer />} />
           <Route path="Seller" element={<Seller />} />

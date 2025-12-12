1e8a1024f2e22077f0cd61aa4bc0ef0042f73adb
Fazri Suhada <fazrisuhada56@gmail.com>
2025-12-12 14:36:20 +0700

    react router : use navigate

--- react-router/src/Home.jsx
+++ react-router/src/Home.jsx
@@ -1,8 +1,19 @@
+import { useNavigate } from "react-router";
+
 export default function Home() {
+
+    const navigate = useNavigate();
+    function handleClick() {
+        navigate({
+            "pathname": "/data"
+        });
+    }
+
     return (
         <div>
             <h1>Home Page</h1>
             <p>Welcome to the Home Page</p>
+            <button onClick={handleClick}>Go to data page</button>
         </div>
     )
 }

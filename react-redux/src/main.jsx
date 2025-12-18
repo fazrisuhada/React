5f164390964a4197777bd64bfecf9511788952f1
Fazri Suhada <fazrisuhada56@gmail.com>
2025-12-18 16:31:41 +0700

    react redux : store

--- react-redux/src/main.jsx
+++ react-redux/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 
-const store = null
+const store = configureStore({
+  reducer: {},
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
